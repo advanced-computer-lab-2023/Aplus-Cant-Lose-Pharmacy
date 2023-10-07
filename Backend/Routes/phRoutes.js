8e4e88479122a9bc8b60a8f91214d0d8682cc90f
Shark23p4 <yelsharkawy42@gmail.com>
2023-10-07 15:40:42 +0300

    Added update medicine

--- Backend/Routes/phRoutes.js
+++ Backend/Routes/phRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router(); // Create an instance of the Express router
 
-const {addPharmacist, addMedicine} = require("../controllers/phController");
+const {addPharmacist, addMedicine,updateMedicineDetails} = require("../controllers/phController");
 
 router.post("/addPharmacist", addPharmacist); 
 router.post("/addMedicine", addMedicine);
+router.put("/updateMedicineDetails", updateMedicineDetails);
 
 module.exports=router;
--- Backend/controllers/phController.js
+++ Backend/controllers/phController.js
@@ -63,6 +63,50 @@ const addMedicine = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+const updateMedicineDetails = async (req, res) => {
+  try {
+    const { oldName, name, activeElement, price } = req.body;
+
+    // Validate that at least one field (name, activeElement, or price) is provided
+    if (!name && !activeElement && !price) {
+      return res.status(400).json({ error: "At least one field (name, activeElement, or price) must be provided for the update." });
+    }
+
+    // Find the medicine by ID
+    const medicine = await Medicine.findOne({name: oldName});
+
+    if (!medicine) {
+      return res.status(404).json({ error: "Medicine not found" });
+    }
+
+    // Update the medicine's details if provided
+    if (name) {
+      const nameExists = await Medicine.findOne({name: name});
+      if (nameExists) {
+        return res.status(400).json({ error: "Medicine already exists" });
+      }
+      medicine.name = name;
+    }
+
+    if (activeElement) {
+      medicine.activeElement = activeElement;
+    }
+
+    // Update the medicine's price if provided
+    if (price) {
+      medicine.price = price;
+    }
+
+    // Save the updated medicine
+    await medicine.save();
+
+    res.status(200).json({ message: "Medicine details updated successfully", medicine });
+  } catch (error) {
+    console.error("Error updating medicine details:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 
-module.exports={addPharmacist, addMedicine};
+
+module.exports={addPharmacist, addMedicine, updateMedicineDetails};
